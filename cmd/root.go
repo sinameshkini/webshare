3f1e6ade06ca421caba59e354f1f388d7c83cb54
Sina Meshkini <meshkini.dev@gmail.com>
2024-06-25 17:34:38 +0330

    impl upload, but not work

--- cmd/root.go
+++ cmd/root.go
@@ -4,19 +4,20 @@ import (
 	"fmt"
 	"github.com/common-nighthawk/go-figure"
 	"github.com/labstack/echo/v4"
+	"github.com/labstack/echo/v4/middleware"
 	"github.com/sinameshkini/webshare/internal/handler"
 	"github.com/sinameshkini/webshare/internal/utils"
 	"github.com/sirupsen/logrus"
 	"github.com/spf13/cobra"
-	"os"
 )
 
 var (
-	port     int
-	path     string
-	username string
-	password string
-	rootCmd  = &cobra.Command{
+	port      int
+	readOnly  string
+	readWrite string
+	username  string
+	password  string
+	rootCmd   = &cobra.Command{
 		Use:   "webshare",
 		Short: "WebShare is a simple and efficient tool for sharing files over HTTP/HTTPS with basic authentication.",
 		Long: `
@@ -27,19 +28,17 @@ var (
 	Whether you need to share files for collaboration or distribute data efficiently,
 	WebShare offers a straightforward solution with minimal setup.`,
 		Run: func(cmd *cobra.Command, args []string) {
-			// Validate directory path
-			info, err := os.Stat(path)
-			if err != nil {
-				logrus.Fatalln(err.Error())
-			}
-			if !info.IsDir() {
-				logrus.Fatalf("Error: %s is not a directory\n", path)
-			}
+			// Validate directory readOnly
 
 			e := echo.New()
 			e.HideBanner = true
 
-			handler.Init(e, path, port, username, password)
+			//e.Use(middleware.Logger())
+			e.Use(middleware.Recover())
+
+			if err := handler.Init(e, readOnly, readWrite, port, username, password); err != nil {
+				logrus.Fatalln(err.Error())
+			}
 
 			address := fmt.Sprintf(":%d", port)
 			e.Logger.Fatal(e.Start(address))
@@ -53,8 +52,9 @@ func init() {
 	fmt.Printf("version: %s \n", utils.GetGitVersion())
 	fmt.Println("Available on https://github.com/sinameshkini/webshare")
 	rootCmd.Flags().IntVarP(&port, "port", "p", 4242, "Specifies the port number on which the web service will run.")
-	rootCmd.Flags().StringVarP(&path, "dir", "d", ".", "Specifies the directory path to be shared.")
-	rootCmd.Flags().StringVarP(&username, "username", "u", "", "Set the username for basic authentication.")
+	rootCmd.Flags().StringVarP(&readOnly, "readonly", "r", ".", "Set the directory that can be accessed for reading.")
+	rootCmd.Flags().StringVarP(&readWrite, "readwrite", "w", "", "Set the directory that can be accessed for both reading and writing.")
+	rootCmd.Flags().StringVarP(&username, "username", "U", "", "Set the username for basic authentication.")
 	rootCmd.Flags().StringVarP(&password, "password", "P", "", "Set the password for basic authentication.")
 }
 
--- internal/handler/handler.go
+++ internal/handler/handler.go
@@ -6,17 +6,31 @@ import (
 	"github.com/labstack/echo/v4"
 	"github.com/labstack/echo/v4/middleware"
 	"github.com/sinameshkini/webshare/internal/utils"
+	"github.com/sirupsen/logrus"
+	"io"
 	"net/http"
+	"os"
 )
 
-type Handler struct{}
+type Handler struct {
+}
 
 var (
-	h *Handler
+	readWriteDir string
+
+	//h *Handler
 )
 
-func Init(e *echo.Echo, path string, port int, username, password string) {
-	h = &Handler{}
+func Init(e *echo.Echo, readOnly, readWrite string, port int, username, password string) (err error) {
+	//h = &Handler{}
+
+	info, err := os.Stat(readOnly)
+	if err != nil {
+		return
+	}
+	if !info.IsDir() {
+		return fmt.Errorf("Error: %s is not a directory\n", readOnly)
+	}
 
 	// basic auth
 	if username != "" && password != "" {
@@ -32,9 +46,25 @@ func Init(e *echo.Echo, path string, port int, username, password string) {
 		fmt.Printf("Access protected by Basic Auth (Username: %s\tPassword: %s)\n", username, password)
 	}
 
-	e.Static("/", path)
+	if readWrite != "" {
+		info, err := os.Stat(readWrite)
+		if err != nil {
+			return err
+		}
+		if !info.IsDir() {
+			return fmt.Errorf("Error: %s is not a directory\n", readWrite)
+		}
+
+		readWriteDir = readWrite
+
+		e.GET("/w", uploadForm)
+		e.POST("/upload", uploadFile)
+		fmt.Printf("Enter this address in other host in local network for upload file: http://%s:%d/w \n", utils.GetIP(), port)
+	}
+
+	e.Static("/", readOnly)
 	e.Use(middleware.StaticWithConfig(middleware.StaticConfig{
-		Root:   path,
+		Root:   readOnly,
 		Browse: true,
 	}))
 	e.GET("/status", func(c echo.Context) error {
@@ -42,4 +72,63 @@ func Init(e *echo.Echo, path string, port int, username, password string) {
 	})
 
 	fmt.Printf("Enter this address in other host in local network: http://%s:%d \n", utils.GetIP(), port)
+
+	return nil
+}
+
+// Handler
+func uploadForm(c echo.Context) error {
+	html := `
+		<!DOCTYPE html>
+		<html>
+		<head>
+			<title>Upload File</title>
+		</head>
+		<body>
+			<h1>Upload File</h1>
+			<form action="/upload" method="post" enctype="multipart/form-data">
+				<input type="file" name="file" />
+				<input type="submit" value="Upload" />
+			</form>
+		</body>
+		</html>`
+	return c.HTML(http.StatusOK, html)
+}
+
+func uploadFile(c echo.Context) error {
+	// Source
+	file, err := c.FormFile("file")
+	if err != nil {
+		return err
+	}
+	src, err := file.Open()
+	if err != nil {
+		return err
+	}
+	defer src.Close()
+
+	// Destination
+	dst, err := createFile(file.Filename)
+	if err != nil {
+		return err
+	}
+	defer dst.Close()
+
+	// Copy
+	if _, err = io.Copy(dst, src); err != nil {
+		return err
+	}
+
+	return c.HTML(http.StatusOK, "<p>File uploaded successfully!</p>")
+}
+
+func createFile(filename string) (*os.File, error) {
+	// Create destination file making sure the path is writeable.
+	filePath := fmt.Sprintf("%s/%s", readWriteDir, filename)
+	logrus.Infof("creating %s file\n", filePath)
+	dst, err := os.Create(filePath)
+	if err != nil {
+		return nil, err
+	}
+	return dst, nil
 }
